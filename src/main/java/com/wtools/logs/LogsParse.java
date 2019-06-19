5b92f3e1973b07e337790b012d94e6b753ad043e
Jerry <jwang@sunray-sh.com>
2019-06-19 17:34:14 +0800

    鲜蘑菇初始化

---
+++ src/main/java/com/wtools/logs/LogsParse.java
@@ -0,0 +1,17 @@
+package com.wtools.logs;
+
+/**
+ * @author Jerry
+ * @commpany Sunray
+ * @since 2019/6/19
+ */
+public class LogsParse {
+
+    public static void main(String[] args) {
+        parseMessageDate();
+    }
+
+    public static void parseMessageDate(){
+        System.out.println("begin");
+    }
+}
