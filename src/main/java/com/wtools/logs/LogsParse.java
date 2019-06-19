fc00b8731ac86a6060cfdb09e643ceefc16f57d7
Jerry <jwang@sunray-sh.com>
2019-06-19 18:17:19 +0800

    增加文件初步处理

--- src/main/java/com/wtools/logs/LogsParse.java
+++ src/main/java/com/wtools/logs/LogsParse.java
@@ -1,5 +1,12 @@
 package com.wtools.logs;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**
  * @author Jerry
  * @commpany Sunray
@@ -7,11 +14,22 @@ package com.wtools.logs;
  */
 public class LogsParse {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         parseMessageDate();
     }
 
-    public static void parseMessageDate(){
-        System.out.println("begin");
+    public static void parseMessageDate() throws IOException {
+        Path sourceFile = Paths.get("D:\\doc\\dizhi\\shpharm.log.2019-06-17.7");
+        Path targetFile = Paths.get("D:\\doc\\dizhi\\shpharm.log.2019-06-17-test.7");
+        Files.lines(sourceFile, Charset.forName("gb2312"))
+                .filter(s -> s.startsWith("2019-06-17 10:41:33"))
+                .forEach(s -> {
+                    try {
+                        Files.write(targetFile, s.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+                        Files.write(targetFile, "\n".getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
