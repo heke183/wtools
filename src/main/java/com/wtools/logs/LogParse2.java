08778102a18b93fb9cbd35e6443c777ce1d2f8b5
Jerry <jwang@sunray-sh.com>
2019-07-24 14:40:23 +0800

    C8
    # Conflicts:
    #	src/main/java/com/wtools/logs/LogParse2.java

--- src/main/java/com/wtools/logs/LogParse2.java
+++ src/main/java/com/wtools/logs/LogParse2.java
@@ -43,6 +43,9 @@ public class LogParse2 {
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
 
     }
 
